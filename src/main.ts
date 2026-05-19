e8271e2a8ebebb2c11ef3dd37b9ebc6269bb0cf3
Nikitas Tsiris <49830817+ntsiris@users.noreply.github.com>
2026-05-19 21:14:32 +0300

    initial scaffold: build setup + empty plugin
    esbuild/tsconfig/manifest and an Obsidian Plugin stub. Nothing functional yet.

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { Plugin } from "obsidian";
+
+export default class PopupDictionaryPlugin extends Plugin {
+	async onload(): Promise<void> {
+		// TODO: selection + hover word lookup
+	}
+
+	onunload(): void {}
+}
